3a0e6cf1941c34664a2856405f707b6a711053c3
princebtr <prince07014@gmail.com>
2024-04-25 12:16:01 +0530

    created login frame of Bank Management System

--- src/main/java/bank/Login.java
+++ src/main/java/bank/Login.java
@@ -1,12 +1,25 @@
 package bank;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Login extends JFrame {
     Login(){
         setTitle("JBank Login");
+        setLayout(null);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/logo.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel label = new JLabel(i3);
+        label.setBounds(70, 10, 100, 100);
+        add(label);
+
+        getContentPane().setBackground(Color.WHITE);
+
         setSize(800,480);
         setVisible(true);
+        setLocation(350, 200);
     }
     public static void main(String[] args) {
         new Login();
