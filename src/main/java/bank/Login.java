74fd76a6721f15faa83b052388fe777fb665c493
princebtr <prince07014@gmail.com>
2024-04-25 11:45:45 +0530

    created login frame of Bank Management System

---
+++ src/main/java/bank/Login.java
@@ -0,0 +1,14 @@
+package bank;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    Login(){
+        setTitle("JBank Login");
+        setSize(800,480);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+}
